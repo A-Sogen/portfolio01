a16a8d3deb6fcd7a2ec41bed696ac4aff5adf2c7
Aoi Sogen <aoooi522@outlook.jp>
2026-04-27 01:42:56 +0900

    クラス分けの調整

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,55 @@
 window.addEventListener('load', () => {
+
   document.body.classList.add('is-show');
-});
 
-document.querySelectorAll('.c-link').forEach(link => {
-  link.addEventListener('click', function(e) {
-    const url = this.getAttribute('href');
-    if (!url || url === '#') return;
-    e.preventDefault();
-    document.body.classList.remove('is-show');
-    setTimeout(() => {
-      window.location.href = url;
-    },500);
-  });
-});
-
-const btn = document.querySelector('.c-menu');
-const nav = document.querySelector('.p-headerMenu--body');
-
-btn.addEventListener('click', function(){
-  this.classList.toggle('is-open');
-  nav.classList.toggle('is-open');
-
-if(this.classList.contains('is-open')) {
-  this.querySelector('.c-menuText').textContent = 'CLOSE';
-  } else {
-  this.querySelector('.c-menuText').textContent = 'MENU';
+  /**
+ * 
+ */
+  function link() {
+    document.querySelectorAll('.c-link').forEach(link => {
+      link.addEventListener('click', function (e) {
+        const url = this.getAttribute('href');
+        if (!url || url === '#') return;
+        e.preventDefault();
+        document.body.classList.remove('is-show');
+        setTimeout(() => {
+          window.location.href = url;
+        }, 500);
+      });
+    });
   }
+
+  /**
+   * ハンバーガーメニューコンテンツボタン制御処理
+   */
+  function changeButtonText() {
+    const btn = document.querySelector('.js-menuToggle');
+    const nav = document.querySelector('.p-headerMenu--body');
+
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      this.classList.toggle('is-open');
+      nav.classList.toggle('is-open');
+    });
+  }
+
+    /**
+   * スクロールで画像拡大
+   */
+
+    /**
+   * スムーススクロールの実装
+   */
+
+    /**
+   * スクロールしたときの画像位置固定？？
+   */
+
+    /**
+   * フッターの画像」
+   */
+  link();
+  changeButtonText();
 });
+
